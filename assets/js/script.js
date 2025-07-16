5f7afb2b3d471998b33fc5977016541c53ac1c2b
DeekshithReddy8374 <madapadeekshithreddy@gmail.com>
2025-07-16 23:58:34 +0530

    Some Changes Have Been Done

--- assets/js/script.js
+++ assets/js/script.js
@@ -114,7 +114,6 @@ for (let i = 0; i < filterBtn.length; i++) {
 }
 
 
-
 // contact form variables
 const form = document.querySelector("[data-form]");
 const formInputs = document.querySelectorAll("[data-form-input]");
@@ -133,7 +132,6 @@ for (let i = 0; i < formInputs.length; i++) {
 
   });
 }
-
 
 
 // page navigation variables
